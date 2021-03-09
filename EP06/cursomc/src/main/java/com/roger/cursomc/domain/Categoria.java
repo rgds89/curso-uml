2fa68f8d4294b57705ea513991dc90d6ff160e28
Roger Silva <roger.silva@matera.com>
2021-03-09 01:06:03 -0300

    Configuracao do banco de dados H2 e criacao automatica da base de dados

--- EP06/cursomc/src/main/java/com/roger/cursomc/domain/Categoria.java
+++ EP06/cursomc/src/main/java/com/roger/cursomc/domain/Categoria.java
@@ -1,13 +1,19 @@
 package com.roger.cursomc.domain;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
     private static final Long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private Integer id;
 
+    @Column
     private String nome;
 
     public Categoria() {
